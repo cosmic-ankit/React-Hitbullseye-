f37f194edc342b050344c9f593ffabb51d6d1574
Ankit Jain <76097952+cosmic-ankit@users.noreply.github.com>
2023-06-13 10:55:56 +0530

    Create Q3_Function.js

---
+++ Q3_Function.js
@@ -0,0 +1,17 @@
+
+const arr = [10, 20, 30, 40];
+
+let sum = 0;
+
+
+    
+    arr.forEach(Add);
+    
+    function Add(element) 
+    {
+        sum = sum + element;
+    }
+    
+        
+    
+console.log("Sum = "+ sum);
